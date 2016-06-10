ba9bb3bc75c9d0a856339bc57ddb67a1634a5cca
ics-ikeda <ikeda@ics-web.jp>
2016-06-10 20:13:15 +0900

    Bootstrap風のフォームを導入

--- app/src/components/property.component.ts
+++ app/src/components/property.component.ts
@@ -6,7 +6,7 @@ declare function require(value:String):any;
 @Component({
 	selector: 'properties',
 	template: `
-    <div>		
+    <div>
 		<ul class="nav nav-tabs">
 		  <li class="nav-item">
 			<a class="nav-link active" href="#tab1" data-toggle="tab">アニメーション設定</a>
@@ -18,13 +18,35 @@ declare function require(value:String):any;
 		
 		<div id="myTabContent" class="tab-content p-x-2">
 			<div class="tab-pane active" id="tab1">
-				<p><label>フレームレート<input type="number" value="30"></label></p>
+			
+			
+				<div class="form-group row">
+					<label class="col-sm-2 form-control-label">フレームレート</label>
+					<div class="col-sm-10">
+						<p class="form-control-static">email@example.com</p>
+					</div>
+				</div>
+				<div class="form-group row">
+					<label for="inputPassword" class="col-sm-2 form-control-label">ループ</label>
+					<div class="col-sm-10">
+						<input type="number" class="form-control" id="inputPassword" placeholder="Password">
+					</div>
+				</div>
+				
+				<div class="form-group row">
+					<label for="inputPassword" class="col-sm-2 form-control-label">ループ回数</label>
+					<div class="col-sm-10">
+						<input type="number" class="form-control" id="inputPassword" placeholder="Password">
+					</div>
+				</div>
+				
+			
+				<p><label><input type="number" value="30"></label></p>
 				
 				<p><label>ループ<input type="checkbox" name="riyu" value="3"></label></p>
 				<p><label>ループ回数<input type="number" value="1"></label></p>
 			</div>
 			<div class="tab-pane" id="tab2">
-				<h1>圧縮方式</h1>
 				<p>zlib</p>
 				<p>7zip <input type="number" value="1"></p>
 				<p>zopfli <input type="number" value="1"></p>
