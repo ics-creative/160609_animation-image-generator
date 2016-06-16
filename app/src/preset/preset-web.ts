26aefa44ce24e189bad1609da3be5cf3c1419214
ics-ikeda <ikeda@ics-web.jp>
2016-06-16 16:38:39 +0900

    プレセット設定 : Web用ではPNG圧縮しない設定

--- app/src/preset/preset-web.ts
+++ app/src/preset/preset-web.ts
@@ -2,15 +2,17 @@ import {CompressionType} from "../type/compression-type";
 import {AnimationImageOptions} from "../data/animation-image-options";
 import {PresetType} from "../type/preset-type";
 
+/**
+ * Webページ用アニメーションのプリセット設定です。
+ */
 export class PresetWeb {
 
 	static setPreset(data:AnimationImageOptions) {
 		data.noLoop = true;
 		data.loop = 4;
-		// data.iterations = 15;
 		data.fps = 30;
 		data.compression = CompressionType.zlib;
-		data.enabledPngCompress = true;
+		data.enabledPngCompress = false;
 		data.enabledWebpCompress = false;
 
 		data.enabledExportApng = true;
