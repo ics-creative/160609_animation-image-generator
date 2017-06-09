02d6d11b700c06f71db34171bf947e50e93c177d
ics-nohara <nozomi.nohara@ics-web.jp>
2017-06-09 18:59:53 +0900

    2→4：行末にセミコロン

--- product/src/app/app.component.ts
+++ product/src/app/app.component.ts
@@ -17,18 +17,18 @@ export class AppComponent implements OnInit {
     const {remote} = require("electron");
     const {Menu, MenuItem} = remote;
 
-    const menu = new Menu()
+    const menu = new Menu();
     menu.append(new MenuItem({
       label:'MenuItem1', click() {
-        console.log('item 1 clicked')
+        console.log('item 1 clicked');
       }
-    }))
+    }));
     menu.append(new MenuItem({type:'separator'}));
     menu.append(new MenuItem({label:'MenuItem2', type:'checkbox', checked:true}));
 
     window.addEventListener('contextmenu', (e) => {
-      e.preventDefault()
-      menu.popup(remote.getCurrentWindow())
+      e.preventDefault();
+      menu.popup(remote.getCurrentWindow());
     }, false)
   }
 }
