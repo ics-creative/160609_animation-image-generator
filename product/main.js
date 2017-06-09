67005360dc39f304f9b9a1677855b38b62ff2f05
ics-nohara <nozomi.nohara@ics-web.jp>
2017-06-09 16:41:03 +0900

    2→4：angular-cliでプロジェクト作成

---
+++ product/main.js
@@ -0,0 +1,49 @@
+// アプリケーション作成用のモジュールを読み込み
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+// メインウィンドウ
+let mainWindow;
+
+function createWindow () {
+  // メインウィンドウを作成します
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // メインウィンドウに表示するURLを指定します
+  // 今回はdistディレクトリのindex.html
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "dist", 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // デベロッパーツールの起動
+  mainWindow.webContents.openDevTools();
+
+  // メインウィンドウが閉じられたときの処理
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+//  初期化が完了した時の処理
+app.on('ready', createWindow);
+
+// 全てのウィンドウが閉じたときの処理
+app.on('window-all-closed', function () {
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+// アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+app.on('activate', function () {
+  /// メインウィンドウが消えている場合は再度メインウィンドウを作成する
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
