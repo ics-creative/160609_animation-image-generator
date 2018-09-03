c27c1d0b0863a38260337a6807913cec3bc0d5d8
Nozomi Nohara <nozomi.nohara@ics-web.jp>
2018-09-03 15:16:53 +0900

    electronのバージョンを更新

--- product/build-scripts/conf.js
+++ product/build-scripts/conf.js
@@ -6,7 +6,7 @@
     '「アニメ画像に変換する君」は連番画像をLINEアニメーションスタンプや、Web用アニメーション画像に変換するアプリケーションです。',
   APP_VERSION: '2.1.6',
   BUILD_VERSION: '2.1.6',
-  ELECTRON_VERSION: '2.0.0',
+  ELECTRON_VERSION: '2.0.8',
   sign: {
     identity: '3rd Party Mac Developer Installer: ICS INC. (53YCXL8YSM)',
     bundleId: 'media.ics.AnimationImageConverter'
