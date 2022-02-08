0565977ff6110b6496b7cc738702da5ee3c0a8dc
ics-matsumoto <matsumoto@ics-web.jp>
2022-02-08 12:03:17 +0900

    問い合わせ番号の生成ロジックを修正

--- product/main-src/generators/createInquiryCode.ts
+++ product/main-src/generators/createInquiryCode.ts
@@ -3,6 +3,6 @@ import { createHash } from 'crypto';
 export const createInquiryCode = (): string => {
   // お問い合わせコード生成
   const hash = createHash('sha256');
-  hash.update(platform + '/' + new Date().toString());
+  hash.update(platform + '/' + String(Date.now()));
   return hash.digest('hex').slice(0, 8);
 };
