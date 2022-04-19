5677b3d84a0aa3e62966e8446d0d752d960fbf3e
ics-nishihara <nishihara@ics-web.jp>
2022-04-19 16:20:13 +0900

    ループ回数を負数にできてしまう不具合修正

--- product/src/app/components/properties/properties.ts
+++ product/src/app/components/properties/properties.ts
@@ -27,15 +27,15 @@ export class PropertiesComponent {
   constructor() {}
 
   avoidBlankLoopNum() {
-    // ループ回数が0やnullの場合は1に補正
-    if (!this.animationOptionData.loop) {
+    // ループ回数が0やnull、負数の場合は1に補正
+    if (!this.animationOptionData.loop || this.animationOptionData.loop < 0) {
       this.animationOptionData.loop = 1;
     }
   }
 
   avoidBlankFpsNum() {
-    // FPS回数が0やnullの場合は1に補正
-    if (!this.animationOptionData.fps) {
+    // FPS回数が0やnull、負数の場合は1に補正
+    if (!this.animationOptionData.fps || this.animationOptionData.fps < 0) {
       this.animationOptionData.fps = 1;
     }
   }
