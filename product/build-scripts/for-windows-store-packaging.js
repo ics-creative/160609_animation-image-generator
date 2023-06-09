40e99cbd857280f5603f4e13415df0a2d63079b4
Nohara Nozomi <nozomi.nohara@ics-web.jp>
2023-06-09 14:26:30 +0900

    Windows kitのパスを環境変数から取得

--- product/build-scripts/for-windows-store-packaging.js
+++ product/build-scripts/for-windows-store-packaging.js
@@ -4,6 +4,13 @@ const conf = require('./conf.js');
 function convertWindowsStore() {
   const electronWindowsStore = require('electron-windows-store');
 
+  // パスはインストール先で変わるので環境変数から取得する
+  if (!process.env.WINDOWS_KIT_PATH) {
+    console.error(`[convert-windows-store] error : WINDOWS_KIT_PATH is not defined. Please set environment variable. 
+      Example:'C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.22000.0\\x64'`);
+    return ;
+  }
+
   electronWindowsStore({
     containerVirtualization: false,
     inputDirectory: `../${conf.EN_NAME}-win32-ia32`,
@@ -12,8 +19,7 @@ function convertWindowsStore() {
     assets: './resources/app-icon/win-icon',
     packageName: `${conf.EN_NAME}`,
     manifest: './AppXmanifest.xml',
-    // Windows Kitへのパスは適宜自身の環境に合わせてください。
-    windowsKit: 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.22000.0/x64',
+    windowsKit: process.env.WINDOWS_KIT_PATH,
     deploy: false,
     finalSay: function () {
       console.log('exit');
