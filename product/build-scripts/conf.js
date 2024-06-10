a6335bd5d98c701805a7c8543eec4019d51fb9b7
Nohara Nozomi <nozomi.nohara@ics-web.jp>
2024-06-10 16:04:21 +0900

    リリースビルド時のelectronバージョンを修正

--- product/build-scripts/conf.js
+++ product/build-scripts/conf.js
@@ -6,7 +6,7 @@
     '「アニメ画像に変換する君」は連番画像をLINEアニメーションスタンプや、Web用アニメーション画像に変換するアプリケーションです。',
   APP_VERSION: '4.3.0',
   BUILD_VERSION: '4.3.0',
-  ELECTRON_VERSION: '28.0.0',
+  ELECTRON_VERSION: '30.0.9',
   distPath: './dist',
   binPath: './bin',
   projectSrcPath: './src',
