081f473dcbee9e951969bcc53de0ca47c638e62c
Nohara Nozomi <nozomi.nohara@ics-web.jp>
2024-06-10 14:28:32 +0900

    electron関連モジュールの更新

--- product/build-scripts/for-mac-packaging.js
+++ product/build-scripts/for-mac-packaging.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { copyRecursiveSync } = require('./copy-recursive-sync.js');
 
 const { join, resolve } = require('path');
-const electronPackager = require('electron-packager');
+const electronPackager = require('@electron/packager');
 const { makeUniversalApp } = require('@electron/universal');
 
 const conf = require('./conf.js');
@@ -35,28 +35,17 @@ const execFlat = () => {
     console.error('No cert config. aborted.');
     return;
   }
-  const flat = require('electron-osx-sign').flat;
+  const { flatAsync } = require('@electron/osx-sign');
   const pkg = `AnimationImageConverter_${signType}.pkg`;
 
-  return new Promise((resolve, reject) => {
-    flat(
-      {
-        app: appPathUniversal,
-        identity: signConfig.flat.identity,
-        pkg: `../${pkg}`,
-        platform: 'mas'
-      },
-      (err) => {
-        if (err) {
-          console.error(err);
-          console.error('flat failure!');
-          reject();
-        } else {
-          console.info('flat done!');
-          resolve();
-        }
-      }
-    );
+  return flatAsync({
+    app: appPathUniversal,
+    identity: signConfig.flat.identity,
+    pkg: `../${pkg}`,
+    platform: 'mas'
+  }).catch((e) => {
+    console.error(e);
+    console.error('flat failure!');
   });
 };
 
@@ -69,33 +58,25 @@ const execSign = () => {
   }
   if (!fs.existsSync(provisioningProfilePath)) {
     console.error(`No provisioning profile found. aborted.
-    Please place the config at "${path.join(__dirname, provisioningProfilePath)}"`);
+    Please place the config at "${path.join(
+      __dirname,
+      provisioningProfilePath
+    )}"`);
     return;
   }
-  const sign = require('electron-osx-sign');
+  const { signAsync } = require('@electron/osx-sign');
 
-  return new Promise((resolve, reject) => {
-    sign(
-      {
-        app: appPathUniversal,
-        entitlements: 'resources/dev/parent.plist',
-        'entitlements-inherit': 'resources/dev/child.plist',
-        platform: 'mas',
-        'provisioning-profile': provisioningProfilePath,
-        type: signType,
-        identity: signConfig.sign.identity
-      },
-      (err) => {
-        if (err) {
-          console.error(err);
-          console.error('sign failure!');
-          reject();
-        } else {
-          console.info('sign done!');
-          resolve();
-        }
-      }
-    );
+  return signAsync({
+    app: appPathUniversal,
+    entitlements: 'resources/dev/parent.plist',
+    'entitlements-inherit': 'resources/dev/child.plist',
+    platform: 'mas',
+    'provisioning-profile': provisioningProfilePath,
+    type: signType,
+    identity: signConfig.sign.identity
+  }).catch((e) => {
+    console.error(e);
+    console.error('sign failure!');
   });
 };
 
