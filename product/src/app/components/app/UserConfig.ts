2dbe7122fa04f3619f9e7a05b091f44972911885
Nohara Nozomi <nozomi.nohara@ics-web.jp>
2024-06-13 17:29:19 +0900

    不必要なimportの整理

--- product/src/app/components/app/UserConfig.ts
+++ product/src/app/components/app/UserConfig.ts
@@ -1,4 +1,3 @@
-import { config } from 'dotenv';
 import { AnimationImageOptions } from '../../../../common-src/data/animation-image-option';
 import { PresetLine } from '../../../../common-src/preset/preset-line';
 import { PresetWeb } from '../../../../common-src/preset/preset-web';
